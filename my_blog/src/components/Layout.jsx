129b3add425638de9c06ac8d629b986118ecb9e3
mrmcmva <vsevolodmartynyuk@yandex.ru>
2025-11-27 10:56:08 +0300

    The application components have been successfully updated to version 2025.11.0

--- my_blog/src/components/Layout.jsx
+++ my_blog/src/components/Layout.jsx
@@ -1,25 +1,28 @@
+import styles2 from './Layout.module.css'
+
 import { Outlet } from "react-router-dom";
 import Header from "./HomePage/Header";
 import Sidebar from "./HomePage/Sidebar";
 import { useGlobalState } from "./GlobalProvider";
+import { useEffect, useState } from "react";
 
 function Layout() {
   const styles = "flex flex-col shadow-sidebar gap-4 min-h-full w-[300px] bg-[#202020ff] items-center"
   const {
-    logStatus
+    profileStatus
   } = useGlobalState();
 
   return (
     // <div id="header"className="flex flex-row min-h-screen min-w-[680px] max-w-[1000px] bg-[#181818ff]">
     <div id="header" className="flex flex-row min-h-screen w-full bg-[#181818ff]">
-        <aside className={logStatus === 'login' ? `${styles}` : 'hidden'}>
+        <aside className={`${profileStatus === 'new' ? `${styles}` : 'hidden'}`}>
             <Sidebar/>
         </aside>
         <div className="flex flex-col w-full">
-            <header>
+            <header className='sticky'>
                 <Header/>
             </header>
-            <main className="m-12">
+            <main className="main">
               <Outlet className="w-full"/>
             </main>
             
